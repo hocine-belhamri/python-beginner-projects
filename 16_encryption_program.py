0e8b1f6db35196f18bdbc303fd95dd229bd29dc1
hocine belhamri <ho.belhamri@gmail.com>
2026-07-08 10:59:34 +0100

    Add encryption program

---
+++ 16_encryption_program.py
@@ -0,0 +1,41 @@
+import random as r
+import string as s
+
+
+chars = s.punctuation + s.digits + s.ascii_letters + " "
+chars = list(chars)
+keys= chars.copy()
+r.shuffle(keys)
+
+option = int(input("enter '1' to encrypt or '2' to decrypt: "))
+while repeat == "":
+
+    #Encrypt
+
+    if option == 1:
+        original_text = input("enter the text u want to encrypt: ")
+        encrypted = ""
+        for char in original_text:
+            i = chars.index(char)
+            encrypted += keys[i]
+        print()
+        print(f"the encrypted: {encrypted}")
+        print(f"the original: {original_text}")
+        repeat = ""
+    #Decrypt
+
+    elif option == 2:
+        encrypted = input("enter the text u want to decrypt: ")
+        decrypted = ""
+        for char in encrypted:
+            i = keys.index(char)
+            decrypted += chars[i]
+        print()
+        print(f"the decrypted: {decrypted}")
+        print(f"the encrypted: {encrypted}")
+    
+    elif option == 3:
+        print("assalam alikum !")
+        break
+    else:
+        print("invalid choice!")
